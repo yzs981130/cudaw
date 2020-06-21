d101996ba9a2cb22ee5fd095f0d5fd5308023155
pkuwxl <wxl@pku.edu.cn>
2020-06-21 13:30:37 +0800

    upload one or more files as arguments

--- upload.sh
+++ upload.sh
@@ -1,9 +1,17 @@
 # /bin/bash
-sudo docker cp ./autotest.sh 720620440bcc:/workspace/rxy-wrapper/autotest.sh
-sudo docker cp ./cudawrt.c 720620440bcc:/workspace/rxy-wrapper/cudawrt.c
-sudo docker cp ./cudawrt.h 720620440bcc:/workspace/rxy-wrapper/cudawrt.h
-sudo docker cp ./vaddr.c 720620440bcc:/workspace/rxy-wrapper/vaddr.c
-sudo docker cp ./vaddr.h 720620440bcc:/workspace/rxy-wrapper/vaddr.h
-sudo docker cp ./targs.c 720620440bcc:/workspace/rxy-wrapper/targs.c
-sudo docker cp ./targs.h 720620440bcc:/workspace/rxy-wrapper/targs.h
-sudo docker cp ./cudawblas.c 720620440bcc:/workspace/rxy-wrapper/cudawblas.c
+
+if [ "$1" != "" ] ; then
+    for f in $* ; do 
+        sudo docker cp $f 720620440bcc:/workspace/rxy-wrapper/
+    done
+    exit
+fi
+
+sudo docker cp ./autotest.sh 720620440bcc:/workspace/rxy-wrapper/
+sudo docker cp ./cudawrt.c 720620440bcc:/workspace/rxy-wrapper/
+sudo docker cp ./cudawrt.h 720620440bcc:/workspace/rxy-wrapper/
+sudo docker cp ./vaddr.c 720620440bcc:/workspace/rxy-wrapper/
+sudo docker cp ./vaddr.h 720620440bcc:/workspace/rxy-wrapper/
+sudo docker cp ./targs.c 720620440bcc:/workspace/rxy-wrapper/
+sudo docker cp ./targs.h 720620440bcc:/workspace/rxy-wrapper/
+sudo docker cp ./cudawblas.c 720620440bcc:/workspace/rxy-wrapper/
